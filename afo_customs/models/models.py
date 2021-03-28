5104ef9e85d764438b0176295fda2123a5c75b2b
Garry Chan <garry@chan.chat>
2021-03-28 21:51:54 +0800

    [ADD UPD] product_dimension and afo customs

--- afo_customs/models/models.py
+++ afo_customs/models/models.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, tools
 
 
 class product_extend(models.Model):
     _name = 'afo_customs.product_extend'
     _description = 'afo_customs.product_extend'
     _inherit = 'product.product'
+
+class contact_extend(models.Model):
+    _inherit = ['res.partner']
+    frontnamecard_image = fields.Image(string="Business Card", max_width='500',max_height='400', verify_resolution=True) 
+    backnamecard_image = fields.Image(string="Back Business Card", max_width='500', max_height='400', verify_resolution=True)
+
 
 
 #     name = fields.Char()
